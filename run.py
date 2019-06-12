071afe5c0f41ecd19445d73ef822cb5d711424e8
abbyshabi <abbyshabi@gmail.com>
2019-06-12 21:48:41 +0300

    included docstrings

--- run.py
+++ run.py
@@ -5,10 +5,16 @@ import requests
 from datetime import datetime
 
 
-
+'''
+Initialization of the sqlite3 datatbase 
+'''
 conn = sqlite3.connect('jenkins_jobs.db') 
 cursor = conn.cursor()
 
+
+'''
+This method initializes the Jenkins connection using the required parameters
+'''
 def init_server(url, username, password):
     jenkins_server = Jenkins(url, username, password)
     return jenkins_server
@@ -20,7 +26,9 @@ password='ambition1'
 
 jenkins_server = init_server(url, username, password)
 
-
+'''
+Loop fetching data from Jenkinsapi using the .get_jobs() method
+'''
 for job, instance in jenkins_server.get_jobs():
 
     
@@ -38,13 +46,21 @@ for job, instance in jenkins_server.get_jobs():
     	cursor.execute('UPDATE jenkins_jobs SET job_status=?, job_date_checked=? WHERE job_name=?', update_list)
 		
 
-
+'''
+commit changes to the database
+'''
 conn.commit()
 
+'''
+Querying the database to display all jobs stored in it
+'''
 cursor.execute('SELECT * from jenkins_jobs')
 jobs = cursor.fetchall()
 for id, jobname, job_status, datechecked in jobs:
 	print(jobname+' || '+job_status+' || '+datechecked)
 
+'''
+End database connection
+''' 
 conn.close()
 	
